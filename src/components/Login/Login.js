d53cf48ef01d0fb5b88f09d0dec407f6268ebe24
arifphero <ahammadarif232@gmail.com>
2021-10-10 13:03:59 +0600

    set private route

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 
 const Login = () => {
     const { signInUsingGoogle} = useAuth();
+    const location = useLocation();
+    console.log('came from', location.state?.from);
     return (
         <div className="login-form">
             <div>
             <h2>Login</h2>
-            <form onSubmit="">
+            <form >
                 <input type="email" name="" id="" placeholder="your email" />
                 <br />
                 <input type="password" name="" id="" /><br />
